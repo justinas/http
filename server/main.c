a1436007655c62795485eee886feace790f54843
Justinas Stankevicius <justinas@justinas.me>
2016-03-04 17:25:54 +0200

    Fully working echo-server.
    No concurrency yet

--- server/main.c
+++ server/main.c
@@ -9,6 +9,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "../parser/parser.h"
+
 void fatal(const char *msg) {
     fputs(msg, stderr);
     exit(1);
@@ -17,6 +19,63 @@ void fatal(const char *msg) {
 void efatal(const char *prepend) {
     perror(prepend);
     exit(1);
+}
+
+void handle_client(int fd) {
+    parser* p = parser_new();
+    for (;;) {
+        char buf[1024];
+        ssize_t n = recv(fd, buf, 1024, 0);
+        if (n == -1) {
+            efatal("recv");
+        } else if (n == 0) {
+            break;
+        }
+        int parser_status = parser_feed(p, buf, n);
+        if (parser_status == -1) {
+            fatal("parser error");
+        }
+        else if (parser_status == 1) {
+            break;
+        }
+    }
+
+    http_request *req = parser_request(p);
+    char *msg = "HTTP/1.0 200 OK\r\n\r\n";
+    char *init_html = "<!doctype html><title>Server</title>\n"
+        "<table>";
+    if (send(fd, msg, strlen(msg), 0) == -1) {
+        efatal("send");
+    }
+    if (send(fd, init_html, strlen(init_html), 0) == -1) {
+        efatal("send");
+    }
+    for (http_header *h = req->header; h != NULL; h = h->next) {
+        char *init = "<tr><td>";
+        if (send(fd, init, strlen(init), 0) == -1) {
+            efatal("send");
+        }
+        if (send(fd, h->name, strlen(h->name), 0) == -1) {
+            efatal("send");
+        }
+        char *mid = "</td><td>";
+        if (send(fd, mid, strlen(mid), 0) == -1) {
+            efatal("send");
+        }
+        if (send(fd, h->value, strlen(h->value), 0) == -1) {
+            efatal("send");
+        }
+        char *end = "</td><td>";
+        if (send(fd, end, strlen(end), 0) == -1) {
+            efatal("</td></tr>");
+        }
+    }
+    char *end_html = "</table>";
+    if (send(fd, end_html, strlen(end_html), 0) == -1) {
+        efatal("</td></tr>");
+    }
+
+    parser_destroy(p);
 }
 
 int main(int argc, char **argv) {
@@ -50,8 +109,7 @@ int main(int argc, char **argv) {
         if (client_fd < 0) {
             efatal("accept");
         }
-        char *msg = "HTTP/1.0 200 OK\r\nContent-Length: 1\r\n\r\ni";
-        write(client_fd, msg, strlen(msg));
+        handle_client(client_fd);
         close(client_fd);
     }
     return 0;
